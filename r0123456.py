6dcda7ba014f633cbd9f4c519fe4e95600d82bba
Thibault Lahaye <thibault.lahaye@outlook.com>
2023-12-24 14:29:52 +0100

    added cyclic_edge_distance & cyclic_rtype_distance

--- r0123456.py
+++ r0123456.py
@@ -41,6 +41,97 @@ def fitness(permutation: np.ndarray, distance_matrix: np.ndarray) -> float:
 
     return cost
 
+@jit(nopython=True)
+def cyclic_edge_distance(permutation_1: np.ndarray, permutation_2: np.ndarray) -> np.int64:    
+    """
+    Calculates the cyclic edge distance between two permutations.
+
+    Cyclic edge distance treats permutations as sets of undirected edges, considering the last element
+    connected to the first. For example, the permutation [1, 5, 2, 4, 0, 3] is equivalent to the set of
+    edges {(1,5), (5,2), (2,4), (4,0), (0,3), (3,1)}.
+
+    The distance between two permutations is the count of edges that differ. It accounts for undirected
+    edges, meaning (i, j) is considered the same as (j, i).
+
+    :param permutation_1: First permutation.
+    :param permutation_2: Second permutation.
+
+    :return: Cyclic edge distance between the two permutations.
+
+    Example:
+    >>> permutation_1 = np.array([1, 5, 2, 4, 0, 3])
+    >>> permutation_2 = np.array([5, 1, 4, 0, 3, 2])
+    >>> cyclic_edge_distance(permutation_1, permutation_2)
+    3
+
+    Runtime: O(n), where n is the length of the permutations.
+
+    Cyclic edge distance was first described in:
+    S. Ronald, "Distance functions for order-based encodings," in Proc. IEEE CEC. IEEE Press, 1997,
+    pp. 49–54.
+
+    Author: Vincent A. Cicirello, https://www.cicirello.org/
+    """
+
+    count_non_shared_edges = np.int64(0)
+    
+    # successors2 = np.empty_like(p2)
+    # for i in range(successors2.size):
+    #     successors2[p2[i]] = p2[(i + 1) % successors2.size]
+    successors2 = np.roll(permutation_2, -1)
+
+    for i in range(successors2.size):
+        j = (i + 1) % successors2.size
+        if permutation_1[j] != successors2[permutation_1[i]] and permutation_1[i] != successors2[permutation_1[j]]:
+            count_non_shared_edges += np.int64(1)
+
+    return count_non_shared_edges
+
+@jit(nopython=True)
+def cyclic_rtype_distance(permutation_1: np.ndarray, permutation_2: np.ndarray) -> np.int64:
+    """
+    Calculates the Cyclic RType distance between two permutations.
+
+    Cyclic RType distance treats permutations as sets of directed edges, considering the last element
+    connected to the first. For example, the permutation [1, 5, 2, 4, 0, 3] is equivalent to the set of
+    directed edges: {(1,5), (5,2), (2,4), (4,0), (0,3), (3,1)}.
+
+    The distance between two permutations is the count of directed edges that differ.
+
+    :param permutation_1: First permutation.
+    :param permutation_2: Second permutation.
+
+    :return: Cyclic RType distance between the two permutations.
+
+    Example:
+    >>> permutation_1 = np.array([1, 5, 2, 4, 0, 3])
+    >>> permutation_2 = np.array([5, 1, 4, 0, 3, 2])
+    >>> cyclic_rtype_distance(permutation_1, permutation_2)
+    4
+
+    Runtime: O(n), where n is the length of the permutations.
+
+    Cyclic RType distance was introduced in:
+    V.A. Cicirello, "The Permutation in a Haystack Problem and the Calculus of Search Landscapes,"
+    IEEE Transactions on Evolutionary Computation, 20(3):434-446, June 2016.
+
+    Author: Vincent A. Cicirello, https://www.cicirello.org/
+    """
+
+    count_non_shared_edges = np.int64(0)
+    
+    # successors2 = np.empty_like(p2)
+    # for i in range(successors2.size):
+    #     successors2[p2[i]] = p2[(i + 1) % successors2.size]
+    successors2 = np.roll(permutation_2, -1)
+
+    for i in range(successors2.size):
+        j = (i + 1) % successors2.size
+        if permutation_1[(i + 1) % successors2.size] != successors2[permutation_1[i]]:
+            count_non_shared_edges += np.int64(1)
+
+    return count_non_shared_edges
+
 # Modify the class name to match your student number.
 class r0713047:
     def __init__(self):
